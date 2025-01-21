7fc03732dea48356f6d48e45233d4ed85decfd70
Mac <mac@Brians-Macbook.lan>
2025-01-21 01:41:46 -0500

    Set Up Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,17 +1,14 @@
 /* eslint-disable no-console */
 // Controller is used to navigate in the web
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log(req.body)
-    res.status(StatusCodes.CREATED).json({ message: 'POST Controller: Create the Board' })
-  } catch (error) {
-    console.log(error)
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+    // console.log(req.body)
+    // res.status(StatusCodes.CREATED).json({ message: 'POST Controller: Create the Board' })
+    throw new ApiError(StatusCodes.BAD_GATEWAY, 'Testing')
+  } catch (error) {next(error)}
 }
 
 export const boardController = {
