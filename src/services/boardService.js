33adb5864ab7fcb40110a8985ea9c16934a36f38
Mac <mac@Brians-Macbook.lan>
2025-01-22 00:54:07 -0500

    Finalize API Create

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 const createNew = async (reqBody) => {
   try {
     // Adjust data for TaskFlow - Add slug for url and SEO
@@ -8,8 +9,13 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
 
+    // Adjust Data at Model
+    const createdBoard = await boardModel.createNew(newBoard)
+
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
     // Always return value for Service
-    return newBoard
+    return getNewBoard
   } catch (error) {
     throw error
   }
